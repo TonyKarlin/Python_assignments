0f8af945a4000e5c90e46502143ca28783fa984b
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2024-01-19 12:25:45 +0200

    Initial commit

--- moduuli02/Tehtävä03.py
+++ moduuli02/Tehtävä03.py
@@ -4,6 +4,6 @@ import math
 
 kanta = float(input("Suorakulmion kanta metreinä: "))
 korkeus = float(input("Suorakulmion korkeus metreinä: "))
-piiri = (kanta ** 2) + (korkeus ** 2)
+piiri = (kanta+kanta) + (korkeus+korkeus)
 pinta_ala = kanta*korkeus
 print(f"Suorakulmion piiri on: {piiri:.2f}m\nSuorakulmion pinta-ala on: {pinta_ala:.2f}m^2")
