f1e7c2f73938eb4b5d4e1eb693c04cbd680a9e59
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2024-01-19 12:07:14 +0200

    Initial commit

---
+++ moduuli02/Tehtävä01.py
@@ -0,0 +1,4 @@
+#Tehtävä01
+
+nimi = input("Mikä sinun nimesi on: ")
+print("Terve, " + nimi + "!")
---
+++ moduuli02/Tehtävä02.py
@@ -0,0 +1,7 @@
+import math
+
+#Tehtävä02
+
+säde = float(input("Mikä on ympyrän säde senttimetreinä: "))
+pinta_ala = math.pi * (säde ** 2)
+print(f"Ympyrän pinta-ala on: {pinta_ala:.2f} cm^2")
