d854050365f3eac328e40ccc2c5fbef3bdb66a25
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2024-01-19 12:42:23 +0200

    Initial commit

---
+++ moduuli02/Tehtävä04.py
@@ -0,0 +1,11 @@
+import math
+
+#Tehtävä04
+
+luku1 = int(input("Ensimmäinen luku:  "))
+luku2 = int(input("Toinen luku: "))
+luku3 = int(input("Kolmas luku: "))
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa / 3
+print(f"Lukujen summa: {summa}\nLukujen tulo: {tulo}\nLukujen keskirvo: {keskiarvo:.2f}")
