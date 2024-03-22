4c7ae8671f2971dad6afb67c1ac8ec4f68f37160
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2024-03-22 13:52:17 +0200

    Initial commit

---
+++ moduuli10/Tehtävä01.py
@@ -0,0 +1,57 @@
+from colorama import Fore
+
+# Tehtävä01
+
+"""Kirjoita Hissi-luokka, joka saa alustajaparametreinaan alimman ja ylimmän kerroksen numeron. Hissillä on metodit
+siirry_kerrokseen, kerros_ylös ja kerros_alas. Uusi hissi on aina alimmassa kerroksessa. Jos tee luodulle hissille h
+esimerkiksi metodikutsun h.siirry_kerrokseen(5), metodi kutsuu joko kerros_ylös- tai kerros_alas-metodia niin monta
+kertaa, että hissi päätyy viidenteen kerrokseen. Viimeksi mainitut metodit ajavat hissiä yhden kerroksen ylös- tai
+alaspäin ja ilmoittavat, missä kerroksessa hissi sen jälkeen on. Testaa luokkaa siten, että teet pääohjelmassa hissin
+ja käsket sen siirtymään haluamaasi kerrokseen ja sen jälkeen takaisin alimpaan kerrokseen."""
+
+
+class Elevator:
+    def __init__(self, elevator_number, ground_floor, top_floor):
+        self.name = elevator_number
+        self.floor = ground_floor
+        self.ground = ground_floor
+        self.top = top_floor
+
+    def up_a_floor(self):
+        if self.floor < 11:
+            self.floor = self.floor + 1
+            print(f"Elevator({elevator1.name}) going up to floor number {self.floor}")
+        else:
+            print(f"{Fore.RED}You're already on the top floor, {self.floor}\n{Fore.RESET}")
+
+    def down_a_floor(self):
+        if self.floor > 1:
+            self.floor = self.floor - 1
+            print(f"Elevator({elevator1.name}) going down to floor number {self.floor}")
+        else:
+            print(f"{Fore.RED}You're already on the bottom floor ({self.floor})\n{Fore.RESET}")
+
+    def move_to_floor(self, choose):
+        if choose == self.floor:
+            print(f"Elevator: {elevator1.name} is already on that floor {self.floor}")
+        elif choose > self.floor:
+            difference = choose - self.floor
+            for e in range(difference):
+                self.up_a_floor()
+        elif choose < self.floor:
+            difference = self.floor - choose
+            for e in range(difference):
+                self.down_a_floor()
+
+    def print_info(self):
+        print(f"Elevator status")
+        print(f"Elevator number: {self.name}\nGround floor: {self.ground}, Top floor: {self.top}\n"
+              f"Current floor: {self.floor}")
+
+
+elevator1 = Elevator(1, 1, 10)
+
+
+elevator1.move_to_floor(5)
+elevator1.move_to_floor(1)
+
